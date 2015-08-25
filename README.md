fe9505f98c31b2ef9ecfd8554edcf21e52e90d5d
Manuel Valls Fernández <manolo@vlrz.es>
2015-08-25 20:18:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# UrlRewriter
+
+## Sample ussage
+
+```javascript
+var UrlRewriter = require('url-rewriter'),
+    urw = new UrlRewriter();
+
+urw.rewrite('/favicon.ico','/.assets/images/favicon/favicon.ico');
+urw.rewrite(/^\/articles\/(.*?)\/.*$/,'/article?id=$1');
+
+console.log(urw.compute('/favicon.ico'));
+// /.assets/images/favicon/favicon.ico
+
+console.log(urw.compute('/articles/58361/foo-bar'));
+// /article?id=58361
+```
+
+## UrlRewriter class
+
+### urw.rewrite( from, to )
+
+Add a rule to the rewriter. If *from* is a `String`, the whole URL is treated as *to*. If it's a `RegExp`, `URL.replace(from, to)` is used instead.
+
+### urw.unrewrite( from )
+
+Undo a previous rewrite call.
+
+### urw.compute( URL )
+
+Transform a URL taking into account previously set rewrite rules.
---
+++ main.js
@@ -0,0 +1,95 @@
+var pct = require('pct'),
+    Target = require('y-emitter').Target,
+    define = require('u-proto/define'),
+
+    from = Symbol(),
+    to = Symbol(),
+    map = Symbol();
+
+// UrlRewriter
+
+function UrlRewriter(){
+
+  this[from] = [];
+  this[to] = [];
+  this[map] = {};
+
+}
+
+UrlRewriter.prototype = Object.create(Target.prototype);
+UrlRewriter.prototype[define]({
+
+  constructor: UrlRewriter,
+
+  compute: function(path){
+    var computed = decode(path,this.maxSlashes),
+        i;
+
+    if(this[map].hasOwnProperty(computed)) computed = this[map][computed];
+    else for(i = 0;i < this[from].length;i++)
+      computed = computed.replace(this[from][i],this[to][i]);
+
+    if(computed != path) return this.compute(computed);
+    return computed;
+  },
+
+  rewrite: function(oldPath,newPath){
+    var i;
+
+    if(oldPath instanceof RegExp){
+
+      i = this[from].indexOf(oldPath);
+
+      if(i == -1){
+        this[from].push(oldPath);
+        this[to].push(newPath);
+      }else this[to][i] = newPath;
+
+    }else this[map][oldPath + ''] = newPath + '';
+
+  },
+
+  unrewrite: function(oldPath){
+    var i;
+
+    if(oldPath instanceof RegExp){
+
+      i = this[from].indexOf(oldPath);
+      if(i != -1){
+        this[from].splice(i,1);
+        this[to].splice(i,1);
+      }
+
+    }else delete this[map][oldPath + ''];
+
+  }
+
+});
+
+// - utils
+
+function decode(url,max){
+  var m = ((url || '') + '').match(/^(.*?)([#\?].*)?$/),
+      path = m[1] || '',
+      rest = m[2] || '',
+      segments = path.split('/',max || 1000),
+      result = [],
+      segment;
+
+  while((segment = segments.shift()) != null) switch(segment){
+    case '..':
+      if(result.length > 1) result.pop();
+    case '.':
+      if(!segments.length) result.push('');
+      break;
+    default:
+      result.push(segment);
+      break;
+  }
+
+  return pct.decode(result.join('/') + rest);
+}
+
+/*/ exports /*/
+
+module.exports = UrlRewriter;
